3ab3d0aea1cd3bc62258906a50ae8031a3d76094
Lokesh Ghosh <lghosh857@gmail.com>
2023-05-04 18:11:13 +0530

    js

---
+++ AnujBhaiya/JS/1.Introduction/script.js
@@ -0,0 +1,8 @@
+let x=90;
+let y=10;
+console.log('The answer is : ',x+y);
+
+let names="Lokesh";
+console.log("My name is  : ",names);
+
+
---
+++ AnujBhaiya/JS/2.Datatypes/script.js
@@ -0,0 +1,46 @@
+//Number
+let a=10;
+
+//String
+a="Lokesh"
+
+//Boolean
+a=false
+
+
+
+
+console.log("a = ",a);
+console.log("The type of a is  ",typeof(a));
+
+a=23;
+a=a+1;
+console.log("The value of a",a);
+console.log("The type of a is  ",typeof(a));
+
+a='23';
+a=a+1;
+console.log("The value of a",a);
+console.log("The type of a is  ",typeof(a));
+
+//Type conversion
+
+//int -> String
+a=10
+a=String(a);
+console.log("The value of a",a);
+console.log("The type of a is  ",typeof(a));
+
+//int -> Boolean
+a=20
+a=Boolean(a);
+console.log("The value of a",a);
+console.log("The type of a is  ",typeof(a));
+
+//String -> Boolean
+a="0"
+a=Boolean(a);
+console.log("The value of a",a);
+console.log("The type of a is  ",typeof(a));
+
+
